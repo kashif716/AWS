3b13f464024e4210d84de592862fb7bda53bf4f8
kashif ali <kashif71659621@gmail.com>
2025-08-27 16:44:30 +0500

    Update10 guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -5,4 +5,4 @@ guess = 5  # You can change this to test
 if guess == secret_number:
     print("You got it!")
 else:
-    print(f"Wrong! The number was {secret_number}")
+    print(f"Wrong test ! The number was {secret_number}")

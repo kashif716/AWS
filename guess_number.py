d4575b55c238f1f2129b0e7c49a9d68a21e75bfc
kashif7165 <your_github_email@example.com>
2025-08-27 12:16:16 +0500

    Add guess_number.py

---
+++ guess_number.py
@@ -0,0 +1,8 @@
+# Guess the number game
+secret_number = 7
+guess = 5  # You can change this to test
+
+if guess == secret_number:
+    print("You got it!")
+else:
+    print(f"Wrong! The number was {secret_number}")

ad18625e0d8da98f8d56ac56b0defed51f962c27
kashif7165 <your_github_email@example.com>
2025-08-28 16:52:33 +0500

    delete all files and folders

--- guess_number.py
+++
@@ -1,8 +0,0 @@
-# Guess the number game
-secret_number = 11
-guess = 5  # You can change this to test
-
-if guess == secret_number:
-    print("You got it!")
-else:
-    print(f"Wrong test ! The number was {secret_number}")
